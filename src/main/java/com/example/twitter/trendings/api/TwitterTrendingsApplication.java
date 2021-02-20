6612aff95cbc3a90392beab92b92885753c4f5e7
Mert Doğan <mert.dogan@trendyol.com>
2021-02-20 17:37:54 +0300

    Project initialization

---
+++ src/main/java/com/example/twitter/trendings/api/TwitterTrendingsApplication.java
@@ -0,0 +1,13 @@
+package com.example.twitter.trendings.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TwitterTrendingsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TwitterTrendingsApplication.class, args);
+	}
+
+}
